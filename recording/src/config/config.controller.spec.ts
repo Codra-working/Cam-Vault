6345eec9c8aab4303fe2cd07d05b06e3078c27ee
Codra-working <codra@outlook.kr>
2026-07-07 13:35:06 +0900

    Remove spec and fix config unit test error

--- recording/src/config/config.controller.spec.ts
+++ recording/src/config/config.controller.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigController } from './config.controller';
-
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration';
 describe('ConfigController', () => {
   let controller: ConfigController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          load: [configuration],
+          isGlobal: true,
+        }),
+      ],
       controllers: [ConfigController],
     }).compile();
 
